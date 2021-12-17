44a3ce190186e4099e2bf1e319acacde1aa72b85
Elena <mesheryakovaelenav@gmail.com>
2021-12-17 13:43:59 +0300

    удалены лишние строчки

--- src/main/java/ru/gb/meshch/hw5/AppData.java
+++ src/main/java/ru/gb/meshch/hw5/AppData.java
@@ -1,13 +1,5 @@
 package ru.gb.meshch.hw5;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class AppData {
 
   private static final String DELIMITER = ";";
