897f61fc5466f6b41e878aefd705ae5cc0d4887e
Elena <mesheryakovaelenav@gmail.com>
2021-12-24 15:53:09 +0300

    Добавлено практическое задание №9

---
+++ src/main/java/ru/gb/meshch/hw9/Data.java
@@ -0,0 +1,15 @@
+package ru.gb.meshch.hw9;
+
+import java.util.List;
+
+public class Data {
+
+  public static HomeWork9.Course getRandomCourse() {
+    return null;
+  }
+
+  public static List<HomeWork9.Student> getStudentsList() {
+
+    return null;
+  }
+}
